6f1b9af9350c52700b1dce852c55e276b576a522
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-08 10:44:57 +0300

    commit tree 2243427c1b450548115b5789193b0e552e910761

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+let scubhq= 42;
+function distanceFromHqInBlocks(distance) {
+return  Maths.abs(distance-scubhq);
+}
+distance(43);
+
+
+function distanceFromHqInFeet(distance) {
+    return (Maths.abs(distance-scubhq) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination -start) * 264;
+}
+
+ function calculatesFarePrice(start, destination) {
+     const range= Math.abs(destination -start) * 264;
+     if (range<=400){
+         return 0;
+     }
+     else if (range>400 && range<=2000){
+         return  (range - 400) *0.02    
+         }
+         else if (range>2000 && range<=2500){
+             return 25
+
+         }
+         else {
+             return 'cannot travel that far'
+         }
+ }
